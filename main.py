42b3a4fd449f5457e7f8817f5ab1d933e92264b4
Jared-FND <example@mail.com>
2023-04-03 19:36:09 -0400

    Clase 3: Obstacles

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from dino_runner.components.game import Game
 if __name__ == "__main__":
     game = Game()
     game.run()
+
+# Tarea
+# Agregar al Bird
+# Tarea++
+# agregar mas obstaculos... (zombies, meteoritos, humanos cn lanzas, ...)
